ae83304216839f38b7cb62f3ce202c5f4b8bdfe2
Andrew Adcock <andrew@4mile.io>
2023-03-02 06:26:09 -0500

    Resolve merge conflicts and generate new report_table.js

--- src/report_table.js
+++ src/report_table.js
@@ -65,8 +65,7 @@ const buildReportTable = function(config, dataTable, updateColumnOrder, element)
       themes[config.layout].use()
     }
   })
-
-
+  
   // Sort group based on sort order from looker
   const sortByColumnSeries = function(group) {
 
