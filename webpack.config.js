e4481e1b087c9d31a613521ab8e99bbbba2e2db4
Jeremy Chang <jeremytchang@google.com>
2023-12-23 00:33:44 +0000

    wip

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const path = require('path');
 module.exports = {
   entry: './src/report_table.js',
   output: {
-    filename: 'report_table.js',
+    filename: 'dist/bundle.js',
     path: path.resolve(__dirname),
   },
   devtool: 'source-map',
